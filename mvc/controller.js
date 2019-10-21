67f70e353df3ea5a4c5666ac701127911878fe14
NeagMircea <56116304+NeagMircea@users.noreply.github.com>
2019-10-21 15:23:19 +0300

    primul_mvc

---
+++ mvc/controller.js
@@ -0,0 +1,33 @@
+class t_eq2_controller
+{
+  view;
+  model;
+  constructor(view,model)
+  {
+    this.view=view;
+    this.model=model;
+    // creeam o lista de functii
+    var events=new t_events();
+    //adaugam on_solve din controller in lista de functii
+    events.add_event("on_solve_clicked_uab",this.on_solve);
+    //transmitem lista de functii la view
+    this.view.set_events(events);
+  }
+
+  on_solve()
+  {
+    var a =this.view.get_a();
+    var b=this.view.get_b();
+    var c=this.view.get_c();
+
+    this.model.set_coefficients();
+    this.model.solve();
+    var solutions=this.model.get_solutions();
+
+    this.view.set_solutions(solutions);
+  }
+
+}
+
+
+var app=new t_eq2_controller(new t_eq2_view(),new t_eq2_model());
