0ecd36996ac7ed44c8b46e938201052e0a737ecc
NeagMircea <56116304+NeagMircea@users.noreply.github.com>
2019-11-25 13:01:20 +0200

    Add files via upload

--- worker/prime.js
+++ worker/prime.js
@@ -1,4 +1,3 @@
-
 function este_prim(n){
 	for (var i = 2; i*i <= n; i++)
 		if (n % i == 0)
@@ -10,7 +9,3 @@ function este_prim(n){
 for (var i = 1E9; i < 1E11; i++)
 	if (este_prim(i))
 		postMessage(i);
-
-
-
-
