dbdaeae5a5142cb51b937e73a5ebbdaf3fb249a0
NeagMircea <56116304+NeagMircea@users.noreply.github.com>
2019-11-25 12:54:22 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,5 +1,4 @@
-  
-document.getElementById("id_logic").innerHTML = "2019.11.25.5";
+document.getElementById("id_logic").innerHTML = "2019.11.25.6";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 
@@ -9,22 +8,26 @@ unghi.valoare = 0;
 
 
 function start(){
-	document.getElementById("id_start").disabled=true;
-	document.getElementById("id_stop").disabled=false;
+	
+	document.getElementById("id_start").disabled = true;
+	document.getElementById("id_stop").disabled = false;
+	
 	timer_id = setInterval(desenare, 20, unghi);
 	
-	var muncitor=new Worker("prime.js");
-	muncitor.onmessage=function(e) { document.getElementById("id_prime").innerHTML=e.data; };
-	
+	var muncitor = new Worker("prime.js");
+	muncitor.onmessage = function(e) {
+		document.getElementById("id_prime").innerHTML = e.data;
+	}		
 }
 
 
-function stop()
-{
-	document.getElementById("id_start").disabled=false;
-	document.getElementById("id_stop").disabled=true;
+function stop(){
+	document.getElementById("id_start").disabled = false;
+	document.getElementById("id_stop").disabled = true;
+	
 	clearInterval(timer_id);
 }
+
 
 
 function desenare(unghi){
@@ -48,6 +51,8 @@ function desenare(unghi){
 			unghi.valoare = 1;
 		}
 }
+
+
 
 
 
